ea0e192c232a7e42ff1d3df9d59053600e058e20
Stonedheart <tytomaszto@gmail.com>
2021-05-23 23:12:41 +0200

    Move font family names list to separate file

--- src/components/legendStylingForm/LegendsStylingForm.tsx
+++ src/components/legendStylingForm/LegendsStylingForm.tsx
@@ -7,61 +7,12 @@ import {
     setIsUppercase
 } from '../../actions/LegendStyleActions';
 import { useLegendStyleContext } from '../../contexts/LegendStyleContext';
+import { fontFamilyNames } from '../../models/legendStyle/legendStyleFontFamilies';
 import { FlexPositions } from '../../models/shared/flexPositions';
 import LegendPositioning from './legendPositioning/LegendPositioning';
 
 import "./LegendsStylingForm.css";
 
-const fontFamilyNames = [
-    'Beyond',
-    'BulletInYourHead',
-    'D3Digitalism',
-    'D3Digitalism-Italic',
-    'DiscoDuck',
-    'DiscoDuck-Italic',
-    'DiscoDuck-Left-Italic',
-    'DiscoDuck-3d',
-    'DiscoDuck-3d-Italic',
-    'DiscoDuck-Chrome',
-    'DiscoDuck-Chrome-Italic',
-    'DiscoDuck-Condensed',
-    'DiscoDuck-Condensed-Italic',
-    'DiscoDuck-Expanded',
-    'DiscoDuck-Expanded-Italic',
-    'DiscoDuck-Gradient',
-    'DiscoDuck-Gradient-Italic',
-    'DiscoDuck-Halftone',
-    'DiscoDuck-Halftone-Italic',
-    'DiscoDuck-Outline',
-    'DiscoDuck-Outline-Italic',
-    'DiscoDuck-Semi-Italic',
-    'Friday13th-Bonus',
-    'Friday13th-SH',
-    'Friday13th-v12',
-    'Justice',
-    'Justice-3d',
-    'Justice-Academy',
-    'Justice-Chrome',
-    'Justice-Condensed',
-    'Justice-Condensed-Outline',
-    'Justice-Condensed-Straight',
-    'Justice-Expanded',
-    'Justice-Expanded-Outline',
-    'Justice-Expanded-Straight',
-    'Justice-Gradient',
-    'Justice-Gradient-Outline',
-    'Justice-Halftone',
-    'Justice-Italic',
-    'Justice-Laser',
-    'Justice-Italic-Left',
-    'Justice-Outline',
-    'Justice-Outline-Italic',
-    'Justice-Semi-Italic',
-    'Justice-Outline-Straight',
-    'Justice-Straight',
-    'Wide-Wake',
-    'Wide-Wake-Black',
-];
 
 const LegendsStylingForm = () => {
     const { dispatch, fontSize } = useLegendStyleContext();
