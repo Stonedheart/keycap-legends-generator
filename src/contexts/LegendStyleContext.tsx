0ae617d27cdffe2cf0038580ff048816a330579c
Stonedheart <tytomaszto@gmail.com>
2021-05-29 21:11:17 +0200

    Use keycaps selection reducer in legend styles context

--- src/contexts/LegendStyleContext.tsx
+++ src/contexts/LegendStyleContext.tsx
@@ -1,25 +1,25 @@
 import { createContext, Dispatch, useContext } from "react";
 import { WithChildren } from "../models/shared/props";
-import { LegendStylePropsState } from "../models/legendStyle/legendStyleState";
 import { ReducerAction } from "../models/shared/reducerAction";
-import { LegendStyleProp } from "../models/legendStyle/legendStyleProp";
-import { useLegendStyleReducer } from "../reducers/legendStyleReducer";
+import { LegendStylePayload } from "../models/legendStyle/legendStylePayload";
+import { KeycapsLegendsStyleState, useLegendStyleReducer } from "../reducers/legendStyleReducer";
 import { LegendStyleActionTypes } from "../models/legendStyle/legendStyleActionTypes";
 
 
-interface LegendStyleContextValue extends LegendStylePropsState {
-    dispatch: Dispatch<ReducerAction<LegendStyleActionTypes, LegendStyleProp>>,
+interface LegendStyleContextValue {
+    keycapsLegendsStylesState: KeycapsLegendsStyleState
+    dispatch: Dispatch<ReducerAction<LegendStyleActionTypes, LegendStylePayload>>,
 }
 
 const LegendStyleContext = createContext({} as LegendStyleContextValue);
 
 const LegendStyleContextProvider = ({ children }: WithChildren) => {
-    const [legendStyleState, dispatch] = useLegendStyleReducer();
+    const [keycapsLegendsStylesState, dispatch] = useLegendStyleReducer();
 
     return (
         <LegendStyleContext.Provider
             value={{
-                ...legendStyleState,
+                keycapsLegendsStylesState,
                 dispatch
             }}
         >
