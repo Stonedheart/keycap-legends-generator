b0796483dde5e3ca9b220bfa503927cfe8888a0f
stonedheart <tytomaszto@gmail.com>
2021-07-21 17:48:29 +0200

    Change file name; change model properties

--- src/models/legendStyle/legendStylePayload.ts
+++
@@ -1,7 +0,0 @@
-import LegendStyleProp from "./legendStyleProp";
-
-export interface LegendStylePayload {
-    legendsIndexes: number[];
-    keycapsIndexes: number[];
-    valueToUpdate: LegendStyleProp;
-}
